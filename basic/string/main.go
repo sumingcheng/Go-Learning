f9637653c1e503b478244e816209015a1f710d25
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-17 15:50:47 +0800

    new: 获取长度

---
+++ basic/string/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	//str := "admin"
+	//
+	//length := len(str)
+	//fmt.Println(length)
+
+	//znStr := "中文"
+	//length := len(znStr)
+	//fmt.Println(length) // 6
+
+	//str := "i am go"
+	//aBytes := []byte(str)
+	//fmt.Println(aBytes)
+	//fmt.Println(len(aBytes))
+
+	//str := "好好学习"
+	//aBytes := []byte(str)
+	//aRunes := []rune(str)
+	//
+	//fmt.Println(len(aBytes)) // 12
+	//fmt.Println(len(aRunes)) // 4
+}

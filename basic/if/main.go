58d61ba4d16023e28b92468e8dfaf122dd342c65
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-17 17:40:29 +0800

    new: add

--- basic/if/main.go
+++ basic/if/main.go
@@ -4,26 +4,39 @@ import "fmt"
 
 func main() {
 	checkScore(90)
-	checkNumber(2)
+	checkScore(80)
+	checkScore(70)
 }
 
-func checkNumber(num int) {
-	switch v := num % 2; v {
-	case 0:
-		fmt.Println(num, "is even")
-	default:
-		fmt.Println(num, "is odd")
-	}
-}
+//func checkNumber(num int) {
+//	switch v := num % 2; v {
+//	case 0:
+//		fmt.Println(num, "is even")
+//	default:
+//		fmt.Println(num, "is odd")
+//	}
+//}
+
+//func checkScore(score int) {
+//	switch {
+//	case score >= 90:
+//		fmt.Println("A grade")
+//		fallthrough
+//	case score >= 80:
+//		fmt.Println("B grade")
+//		fallthrough
+//	case score >= 70:
+//		fmt.Println("C grade")
+//		fallthrough
+//	default:
+//		fmt.Println("D grade")
+//	}
+//}
 
 func checkScore(score int) {
 	switch {
-	case score >= 90:
+	case score >= 90, score >= 80, score >= 70:
 		fmt.Println("A grade")
-	case score >= 80:
-		fmt.Println("B grade")
-	case score >= 70:
-		fmt.Println("C grade")
 	default:
 		fmt.Println("D grade")
 	}

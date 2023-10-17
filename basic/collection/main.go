458df790fe37f412288bd1314cbf687d86330f65
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-17 22:00:56 +0800

    new: array

---
+++ basic/collection/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	//var arr [3]string
+	//arr[1] = "字符串1"
+	//
+	//fmt.Print(arr)
+
+	/*	array1 := [3]int{1, 2, 3}
+		array2 := [3]int{1, 2, 3}
+		array3 := [3]int{4, 5, 6}
+
+		fmt.Println(array1 == array2) // 输出：true
+		fmt.Println(array1 == array3) // 输出：false*/
+
+	//a := [3]int{1, 2, 3}
+	//b := a         // a 的值被复制到 b
+	//b[0] = 42      // 修改 b 不会影响 a
+	//fmt.Println(a) // 输出：[1 2 3]
+	//fmt.Println(b) // 输出：[42 2 3]
+
+}

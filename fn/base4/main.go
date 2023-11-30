a477d2981554d836199b8466741a2109b7245a4a
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-11-30 14:41:59 +0800

    feat: 给类型增加方法

--- fn/base4/main.go
+++ fn/base4/main.go
@@ -1,5 +1,27 @@
 package main
 
 func main() {
+	var slice ComputeSlice = []int{1, 2}
+	println(slice.Sum())
+	println(slice.Sub())
+	println(slice.Mul())
+	println(slice.Div())
+}
 
+type ComputeSlice []int
+
+func (cs ComputeSlice) Sum() int {
+	return cs[0] + cs[1]
+}
+
+func (cs ComputeSlice) Sub() int {
+	return cs[0] - cs[1]
+}
+
+func (cs ComputeSlice) Mul() int {
+	return cs[0] * cs[1]
+}
+
+func (cs ComputeSlice) Div() int {
+	return cs[0] / cs[1]
 }

24ea94b60c1a09cc418cd82a06e8c44fb447ebc4
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-12-01 10:32:51 +0800

    feat: 类型和类型别名

---
+++ structure/base1/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// 使用类型，简化参数
+	type TypeCallBack func(a, b int, sign string, res int) string
+	//compute := func(a, b int, method string, cb TypeCallBack) string {
+	//	return cb(a, b, method, a+b)
+	//}
+}

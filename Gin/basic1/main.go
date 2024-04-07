03e85dece6b1d9dbeb84de5815b101cea83548be
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-04-07 20:44:27 +0800

    fix: 服务端渲染

---
+++ Gin/basic1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("当前工作目录:", dir)
+
+	router := gin.Default()
+
+	router.LoadHTMLGlob("./Gin/basic1/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Title":   "主页",
+			"Header":  "欢迎来到我的网站",
+			"Content": "这是使用Gin进行服务端渲染的示例。",
+		})
+	})
+
+	router.Run(":8080")
+}

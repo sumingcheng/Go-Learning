b82f84b5f2b16bfdee1490afe52540710f5c25a8
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-04-18 21:07:18 +0800

    fix: validator

--- RPC/Bind/main.go
+++ RPC/Bind/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
+// 创建一个验证器实例
+var validate = validator.New()
+
 type Header struct {
-	ContentType string `header:"Content-Type"`
-	UserAgent   string `header:"User-Agent"`
+	ContentType string `header:"Content-Type" validate:"required"` // 校验必须是 JSON 类型
+	UserAgent   string `header:"User-Agent" validate:"required"`
 }
 
 func main() {
@@ -15,13 +19,19 @@ func main() {
 
 	router.GET("/test", func(c *gin.Context) {
 		var h Header
-		// 绑定 Header 到结构体
 		if err := c.ShouldBindHeader(&h); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
+		// 进行 Header 验证
+		if err := validate.Struct(h); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
 		c.JSON(http.StatusOK, gin.H{"Content-Type": h.ContentType, "User-Agent": h.UserAgent})
 	})
 
-	router.Run(":8080")
+	router.Run(":8888")
 }

abfa491e4e71bcc66e7b779777fa8f87264a98b6
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-04-26 10:02:28 +0800

    fix: 空切片

---
+++ ExerciseQuestions/nil.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s1 []int
+	//var s2 []int{}
+
+	if s1 == nil {
+		fmt.Println("yes nil")
+	} else {
+		fmt.Println("no nil")
+	}
+
+	//if s2 == nil {
+	//	fmt.Println("yes nil")
+	//} else {
+	//	fmt.Println("no nil")
+	//}
+}

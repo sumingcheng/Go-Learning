13428949d0850eb4a508d30f4143a9db8406ab91
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-04-29 17:47:40 +0800

    fix: 方法别名

---
+++ ExerciseQuestions/structure6.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type User struct {
+}
+
+type User1 User
+type User2 = User
+
+func (i User1) m1() {
+	fmt.Println("m1")
+}
+
+func (i User2) m2() {
+	fmt.Println("m2")
+}
+
+func main() {
+	var i1 User1
+	var i2 User2
+
+	i1.m1()
+	i2.m2()
+}

16a03bcbaedb2b90dddbc978b0b793381c4da3d0
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-05-15 09:58:03 +0800

    fix: 内部作用域

---
+++ ExerciseQuestions/defer13.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func watShadowDefer(i int) (ret int) {
+	ret = i * 2
+	if ret > 10 {
+		ret := 10
+		defer func() {
+			ret = ret + 1
+		}()
+	}
+
+	return
+}
+
+func main() {
+	result := watShadowDefer(50)
+	fmt.Println(result)
+}

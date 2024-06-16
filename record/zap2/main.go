4b808eb392796c2b937758794aeed8f15e8b9126
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-06-16 20:06:12 +0800

    fix: 使用 lumberjack 管理日志

--- record/zap2/main.go
+++ record/zap2/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
+	"strconv"
 )
 
 func main() {
+	// 配置 lumberjack 作为 zap 的日志输出文件
+	logWriter := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "./zap2/logs/log-info.log", // 日志文件的位置
+		MaxSize:    1,                          // 文件大小限制，单位：MB
+		MaxBackups: 3,                          // 最多保留3个备份
+		MaxAge:     28,                         // 文件最多保存28天
+		Compress:   true,                       // 是否压缩/归档旧文件
+	})
+
 	config := zap.NewProductionEncoderConfig()
 
 	config.EncodeTime = zapcore.ISO8601TimeEncoder
@@ -28,7 +39,7 @@ func main() {
 	}
 
 	debugFile, _ := os.OpenFile(fileMap["DEBUG"], fileFlag, prem)
-	infoFile, _ := os.OpenFile(fileMap["INFO"], fileFlag, prem)
+	//infoFile, _ := os.OpenFile(fileMap["INFO"], fileFlag, prem)
 	warnFile, _ := os.OpenFile(fileMap["WARN"], fileFlag, prem)
 	errorFile, _ := os.OpenFile(fileMap["ERROR"], fileFlag, prem)
 	panicFile, _ := os.OpenFile(fileMap["PANIC"], fileFlag, prem)
@@ -37,7 +48,7 @@ func main() {
 
 	teeCore := zapcore.NewTee(
 		zapcore.NewCore(fileEncoder, zapcore.AddSync(debugFile), zapcore.DebugLevel),
-		zapcore.NewCore(fileEncoder, zapcore.AddSync(infoFile), zapcore.InfoLevel),
+		zapcore.NewCore(fileEncoder, zapcore.AddSync(logWriter), zapcore.InfoLevel),
 		zapcore.NewCore(fileEncoder, zapcore.AddSync(warnFile), zapcore.WarnLevel),
 		zapcore.NewCore(fileEncoder, zapcore.AddSync(errorFile), zapcore.ErrorLevel),
 		zapcore.NewCore(fileEncoder, zapcore.AddSync(panicFile), zapcore.PanicLevel),
@@ -49,15 +60,18 @@ func main() {
 
 	defer logger.Sync()
 
-	logger.Info("这是一个普通日志",
-		zap.Namespace("name"),
-		zap.String("name", "zhangsan"),
-		zap.Int("age", 18),
-	)
-	logger.Error("这是一个错误日志")
-	logger.Debug("这是一个调试日志")
-	logger.Warn("这是一个警告日志")
-	logger.Panic("这是一个panic日志")
-	logger.DPanic("这是一个dpanic日志")
-	logger.Fatal("这是一个fatal日志")
+	//logger.Error("这是一个错误日志")
+	//logger.Debug("这是一个调试日志")
+	//logger.Warn("这是一个警告日志")
+	//logger.Panic("这是一个panic日志")
+	//logger.DPanic("这是一个dpanic日志")
+	//logger.Fatal("这是一个fatal日志")
+
+	for i := 0; i < 100000; i++ {
+		logger.Info("这是一个普通日志 - "+strconv.Itoa(i),
+			zap.Namespace("name"),
+			zap.String("name", "zhangsan"),
+			zap.Int("age", 18),
+		)
+	}
 }

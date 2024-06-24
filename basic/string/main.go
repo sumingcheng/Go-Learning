d3fa7519e6a187390d8c36a0fa7c3bbd46bad476
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-06-24 20:20:35 +0800

    fix: 字符串切片

--- basic/string/main.go
+++ basic/string/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	//str := "admin"
 	//
@@ -27,4 +29,13 @@ func main() {
 	//fmt.Println(`他说:"穷是原罪"`) // 他说:"穷是原罪"
 
 	//fmt.Println("Hello\aWorld")
+
+}
+
+func Str() {
+	s := "Z中"
+	brr := []byte(s)
+	fmt.Println(len(s), len(brr)) // 4 4
+	fmt.Println(brr)              // [90 228 184 173] 说明一个中文字符占3个字节 228 184 173 = 中
+	fmt.Println(brr[2], s[2])     // 184 184  默认情况下把字符串当做 byte 切片来使用
 }

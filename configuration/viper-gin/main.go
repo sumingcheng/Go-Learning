91bb84987019c76396536bae5e940829ff176275
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-07-09 21:27:06 +0800

    fix: viper

--- configuration/viper-gin/main.go
+++ configuration/viper-gin/main.go
@@ -27,23 +27,36 @@ type Config struct {
 
 var cfg = new(Config)
 
+// Initialize 函数用于初始化配置，返回配置结构体指针和错误。
 func Initialize() (*Config, error) {
-	viper.SetConfigFile("configuration/viper-gin/config.yaml")
+	// 设置 Viper 来读取指定路径下的 YAML 配置文件。
+	viper.SetConfigFile("configuration/viper-gin/config/config.yaml")
+
+	// 设置 Viper 监听配置文件的更改。
 	viper.WatchConfig()
+
+	// 定义配置文件发生变化时的回调函数。
 	viper.OnConfigChange(func(e fsnotify.Event) {
+		// 当配置文件发生变化时，重新从配置文件中解析数据到 cfg 结构体。
 		if err := viper.Unmarshal(&cfg); err != nil {
+			// 如果解析过程出错，记录致命错误并退出程序。
 			log.Fatalf("解析配置文件失败：%s", err)
 		}
 	})
 
+	// 尝试读取配置文件。
 	if err := viper.ReadInConfig(); err != nil {
+		// 如果读取配置文件失败，返回错误。
 		return nil, err
 	}
 
+	// 解析配置文件内容到 cfg 结构体。
 	if err := viper.Unmarshal(&cfg); err != nil {
+		// 如果解析配置失败，返回错误。
 		return nil, err
 	}
 
+	// 如果配置文件成功被读取和解析，返回 cfg 结构体指针和 nil 错误。
 	return cfg, nil
 }
 
